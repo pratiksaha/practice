c5940ae4bc76d45f4fea9c28ef2b5ef226adff7b
pratiksaha <pratiksaha89@gmail.com>
2014-10-23 11:51:55 +0530

    lcs

--- dynamicprogramming/longestCommonSubsequence.cpp
+++ dynamicprogramming/longestCommonSubsequence.cpp
@@ -9,9 +9,7 @@ void lcs( char *X, char *Y) {
     int m = strlen(X);
     int n = strlen(Y);
     int i, j, L[m+1][n+1];
- 
-   /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+
     for (i=0; i<=m; i++) {
         for (j=0; j<=n; j++) {
             if (i == 0 || j == 0)
