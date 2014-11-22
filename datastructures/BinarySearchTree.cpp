c32b0ea879ad778b9e9dd5b19c762c480ed6341d
pratiksaha <pratiksaha89@gmail.com>
2014-11-22 22:31:05 +0530

    bst

--- datastructures/BinarySearchTree.cpp
+++ datastructures/BinarySearchTree.cpp
@@ -101,49 +101,6 @@ struct node* floorBST(struct node *node, int key) { //find floor for the key in 
     }
 }
 
-struct node* inorderPredecessor(struct node *node, struct node *key_node) { //find inorder predecessor node
-    if(node == NULL)
-        return NULL;       
-    if(key_node->left != NULL) {
-        return maxBST(key_node->left);
-    } else {
-        struct node *temp = NULL;
-        while (node != NULL) {
-            if (key_node->data < node->data) {
-                temp = node;
-                node = node->left;
-            } else if (key_node->data > node->data) {
-                node = node->right;
-            } else {
-                break;
-            }
-        }
-        return temp;
-    }
-}
-
-struct node* inorderSuccessor(struct node *node, struct node *key_node) { //find inorder successor node
-    if(node == NULL)
-        return NULL;
-        
-    if(key_node->right != NULL) {
-        return minBST(key_node->right);
-    } else {
-        struct node *temp = NULL;
-        while (node != NULL) {
-            if (key_node->data > node->data) {
-                temp = node;
-                node = node->right;
-            } else if (key_node->data < node->data) {
-                node = node->left;
-            } else {
-                break;
-            }
-        }
-        return temp;
-    }
-}
-
 struct node* kSmallest(struct node *node, int k, int* curr) { //find kth smallest node
     if(node) {
         struct node *temp = kSmallest(node->left, k, curr);
@@ -172,13 +129,62 @@ struct node* kLargest(struct node *node, int k, int *curr) { //find kth largest 
     }
 }
 
+struct node* removeOutsideRange(struct node *node, int minKey, int maxKey) { //remove nodes outside minKey and maxKey
+    if(node) {
+        node->left = removeOutsideRange(node->left, minKey, maxKey);
+        node->right = removeOutsideRange(node->right, minKey, maxKey);
+        if (node->data < minKey) {
+            struct node *rChild = node->right;
+            free(node);
+            return rChild;
+        } else if (node->data > maxKey) {
+            struct node *lChild = node->left;
+            free(node);
+            return lChild;
+        } else {
+            return node; //node is in range
+        }
+    } else {
+        return NULL;
+    }
+}
+
+void inorderPreSuc(struct node *node, struct node *key, struct node **pre, struct node **suc) {//get the inorder predecessor & successor
+    if(node) {
+        if (node->data < key->data) { //goto right if key is greater than node
+            *pre = node; //predecessor = node
+            inorderPreSuc(node->right, key, pre, suc);
+        } else if(node->data > key->data) { //goto left if key is smaller than node
+            *suc = node; //successor = node
+            inorderPreSuc(node->left, key, pre, suc);
+        } else {
+            if(node->left)
+                *pre = maxBST(node->left); //max in left subtree is predecessor
+            if(node->right)
+                *suc = minBST(node->right); //min in right subtree is successor
+        }
+    }
+}
+
+struct node *lowestCommonAncestor(struct node* node, int key1, int key2) { //find the lowest common ancestor node
+    while (node != NULL) {
+        if (node->data > key1 && node->data > key2) //if both key1 and key2 are smaller than node, then LCA lies in left
+            node = node->left;
+        else if (node->data < key1 && node->data < key2) //if both key1 and key2 are greater than root, then LCA lies in right
+            node = node->right;
+        else //current node is the LCA
+            break;
+    }
+    return node;
+}
+
 struct node* searchBST(struct node *node, int key) { //search key in the BST
-    if(node == NULL || node->data == key)
-        return node;
-    else if(node->data < key)
+    if(node && node->data < key)
         return searchBST(node->right, key);
-    else
+    else if(node && node->data > key)
         return searchBST(node->left, key);
+    else //node is NULL or node->data == key
+        return node;
 }
 
 void insertNode(struct node **node, int key) { //BST insert
@@ -195,9 +201,38 @@ void insertNode(struct node **node, int key) { //BST insert
     }
 }
 
+struct node* deleteNode(struct node* node, int key) {
+    if(node) {
+        if (node->data < key) {
+            node->right = deleteNode(node->right, key);
+        } else if (node->data > key) {
+            node->left = deleteNode(node->left, key);
+        } else {
+            if(!node->left && !node->right) {
+                free(node);
+                return NULL;
+            } else if(!(node->left)) {
+                struct node *temp = node->right;
+                free(node);
+                return temp;
+            } else if(!(node->right)) {
+                struct node *temp = node->left;
+                free(node);
+                return temp;
+            } else {
+                struct node* temp = minBST(node->right);
+                node->data = temp->data;
+                node->right = deleteNode(node->right, temp->data);
+            }
+        }
+    }
+    return node; //either node is NULL or deleted node had 2 children
+}
+
 int main() {
     struct node *t = NULL;
     struct node *temp = NULL;
+    struct node *p, *s;
     int tmp = 0;
     insertNode(&t, 50);
     insertNode(&t, 25);
@@ -231,8 +266,6 @@ int main() {
     cout<<"Floor of 49 = "<<floorBST(t, 49)->data<<endl; 
     cout<<"Ceil of 51 = "<<ceilBST(t, 51)->data<<endl;
     cout<<"Floor of 51 = "<<floorBST(t, 51)->data<<endl;
-    temp = inorderSuccessor(t, t), cout<<"Inorder Successor of 50 = ", (temp)?cout<<temp->data<<endl:cout<<"Not exist"<<endl;
-    temp = inorderPredecessor(t, t), cout<<"Inorder Predecessor of 50 = ", (temp)?cout<<temp->data<<endl:cout<<"Not exist"<<endl;
     tmp = 0, temp = kSmallest(t, 0, &tmp), cout<<"0th smallest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
     tmp = 0, temp = kLargest(t, 0, &tmp), cout<<"0th largest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
     tmp = 0, temp = kSmallest(t, 17, &tmp), cout<<"17th smallest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
@@ -245,6 +278,18 @@ int main() {
     tmp = 0, temp = kLargest(t, 2, &tmp), cout<<"2th largest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
     tmp = 0, temp = kSmallest(t, 8, &tmp), cout<<"8th smallest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
     tmp = 0, temp = kLargest(t, 8, &tmp), cout<<"8th largest in tree= ", (temp)?cout<<temp->data<<endl:cout<<"Not exist "<<endl;
+    cout<<"Removing nodes outside 2&89", t = removeOutsideRange(t, 2, 89); cout<<" Tree:", printTree(t), cout<<endl;
+    inorderPreSuc(t, t, &p, &s), cout<<"For 50", (p)?cout<<" Pre :"<<p->data:cout<<"NA", (s)?cout<<" Suc :"<<s->data:cout<<"NA", cout<<endl;
+    inorderPreSuc(t, t->left, &p, &s), cout<<"For 25", (p)?cout<<" Pre :"<<p->data:cout<<"NA", (s)?cout<<" Suc :"<<s->data:cout<<"NA", cout<<endl;
+    inorderPreSuc(t, t->right, &p, &s), cout<<"For 75", (p)?cout<<" Pre :"<<p->data:cout<<"NA", (s)?cout<<" Suc :"<<s->data:cout<<"NA", cout<<endl;
+    inorderPreSuc(t, t->left->right->right, &p, &s), cout<<"For 45", (p)?cout<<" Pre :"<<p->data:cout<<"NA", (s)?cout<<" Suc :"<<s->data:cout<<"NA", cout<<endl;
+    inorderPreSuc(t, t->right->left->left, &p, &s), cout<<"For 55", (p)?cout<<" Pre :"<<p->data:cout<<"NA", (s)?cout<<" Suc :"<<s->data:cout<<"NA", cout<<endl;
+    cout<<"Lowest Common Ancestor of 5 & 45 :", temp = lowestCommonAncestor(t, 5, 45), (temp)?cout<<temp->data:"NA", cout<<endl;
+    cout<<"Deleting 45", t = deleteNode(t, 45), cout<<" Tree:", printTree(t), cout<<endl;
+    cout<<"Deleting 55", t = deleteNode(t, 55), cout<<" Tree:", printTree(t), cout<<endl;
+    cout<<"Deleting 5", t = deleteNode(t, 5), cout<<" Tree:", printTree(t), cout<<endl;
+    cout<<"Deleting 60", t = deleteNode(t, 60), cout<<" Tree:", printTree(t), cout<<endl;
+    cout<<"Deleting 50", t = deleteNode(t, 50), cout<<" Tree:", printTree(t), cout<<endl;
     deleteTree(&t), cout<<"Deleted Tree :", printTree(t),cout<<endl;
     return 0;
 }
