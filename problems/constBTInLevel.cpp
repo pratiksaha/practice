3761d5535d4ba216dabb94ac8385939317bc5a7b
pratiksaha <pratiksaha89@gmail.com>
2014-12-08 22:37:37 +0530

    const

---
+++ problems/constBTInLevel.cpp
@@ -0,0 +1,72 @@
+//construct BT from inorder and preorder traversal
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+struct node {
+    int data;
+    struct node* left;
+    struct node* right;
+};
+ 
+struct node * getNode(int key) { //allocate memory and initialize
+    struct node * temp = (struct node *)malloc(sizeof(struct node));
+    if(temp == NULL) {
+        cout<<"Memory full\n";
+        exit(1);
+    }
+    temp->data = key;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+void printTree(struct node *node) { //print the tree inorder
+    if (node) {
+        printTree(node->left);
+        cout<<" "<<node->data;
+        printTree(node->right);
+    }
+}
+
+int search(int arr[], int start, int end, char value) {
+    for(int i=start; i<=end; i++)
+        if(arr[i] == value)
+            return i;
+    return -1;
+}
+
+int *extractKeys(int in[], int level[], int m, int n) {
+    int *newlevel = new int[m];
+    int j = 0;
+    for (int i = 0; i < n; i++)
+        if (search(in, 0, m-1, level[i]) != -1)
+            newlevel[j] = level[i], j++;
+    return newlevel;
+}
+
+struct node* buildTree(int in[], int level[], int start, int end, int n) {
+    if (start > end)
+        return NULL;
+    struct node *temp = getNode(level[0]);
+    if (start == end)
+        return temp;
+    int inIndex = search(in, start, end, temp->data);
+    int *llevel  = extractKeys(in, level, inIndex, n);
+    int *rlevel  = extractKeys(in+inIndex+1, level, n-inIndex-1, n);
+    temp->left = buildTree(in, llevel, start, inIndex-1, n);
+    temp->right = buildTree(in, rlevel, inIndex+1, end, n);
+    delete[] llevel;
+    delete[] rlevel;
+    return temp;
+}
+
+
+int main() {
+    int in[] = {4, 8, 10, 12, 14, 20, 22};
+    int level[] = {20, 8, 22, 4, 12, 10, 14};
+    int n = sizeof(in)/sizeof(in[0]);
+    struct node *root = buildTree(in, level, 0, n-1, n);
+    cout<<"Tree :", printTree(root), cout<<endl;
+    return 0;
+}
--- problems/constBTInPost.cpp
+++ problems/constBTInPost.cpp
@@ -1,4 +1,4 @@
-//construct BT from inorder and preorder traversal
+//construct BT from inorder and postorder traversal
 #include<iostream>
 #include<cstdlib>
 using namespace std;
@@ -39,25 +39,25 @@ int search(char arr[], int start, int end, char value) {
 struct node* buildTreeUtil(char in[], char pre[], int start, int end, int *ind) {
     if(start > end)
         return NULL;
-    struct node *temp = getNode(pre[(*ind)++]);
+    struct node *temp = getNode(post[(*ind)--]);
     if(start == end)
         return temp;
     int inIndex = search(in, start, end, temp->data);
-    temp->left = buildTreeUtil(in, pre, start, inIndex-1, ind);
-    temp->right = buildTreeUtil(in, pre, inIndex+1, end, ind);
+    temp->left = buildTreeUtil(in, post, start, inIndex-1, ind);
+    temp->right = buildTreeUtil(in, post, inIndex+1, end, ind);
     return temp;
 }
 
-struct node *buildTree(char in[], char pre[], int size) {
-    int preIndex = 0;
-    return buildTreeUtil(in, pre, 0, size-1, &preIndex);
+struct node *buildTree(char in[], char post[], int size) {
+    int postIndex = size-1;
+    return buildTreeUtil(in, post, 0, size-1, &postIndex);
 }
 
 
 int main() {
     char in[] = {'D', 'B', 'E', 'A', 'F', 'C'};
-    char pre[] = {'A', 'B', 'D', 'E', 'C', 'F'};
-    struct node *root = buildTree(in, pre,sizeof(in)/sizeof(in[0]));
+    char post[] = {'B', 'E', 'D', 'F', 'C', 'A'};
+    struct node *root = buildTree(in, post, sizeof(in)/sizeof(in[0]));
     cout<<"Tree :", printTree(root), cout<<endl;
     return 0;
 }
