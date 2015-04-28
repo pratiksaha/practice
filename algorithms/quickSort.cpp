e7eb170450f5bd6400dcb1e7e114fb7ce4190732
pratiksaha <pratiksaha89@gmail.com>
2015-04-28 19:52:08 +0530

    quicksort iterative

--- algorithms/quickSort.cpp
+++ algorithms/quickSort.cpp
@@ -24,6 +24,26 @@ void quickSort(int arr[], int l, int r) { //CLRS quick sort
     }
 }
 
+void quickSortIterative(int arr[], int l, int r) {
+    int stack[r-l+1];
+    int top = -1;
+    stack[++top] = l;
+    stack[++top] = r;
+    while (top>=0) {
+        r = stack[top--];
+        l = stack[top--];
+        int p = partition(arr, l, r);
+        if (p-1>l) { //push left side to stack if there are elems on left side of pivot
+            stack[++top] = l;
+            stack[++top] = p-1;
+        }
+        if (p+1<r) { //push right side to stack if there are elems on right side of pivot
+            stack[++top] = p+1;
+            stack[++top] = r;
+        }
+    }
+}
+
 void printArray(int arr[], int size) {
     for (int i=0; i<size; i++)
         cout<<" "<<arr[i];
@@ -38,5 +58,12 @@ int main() {
     quickSort(a1, 0, n1-1);
     cout<<"Sorted Array :";
     printArray(a1, n1);
+    int a2[] = {10, 7, 8, 9, 1, 5};
+    int n2 = sizeof(a2)/sizeof(a2[0]);
+    cout<<"Input Array :";
+    printArray(a2, n2);
+    quickSortIterative(a2, 0, n2-1);
+    cout<<"Sorted Array :";
+    printArray(a2, n2);
     return 0;
 }
